ccfdcc488963e8fe43f9795c0391733dfb1efb12
renatocamp <renatocontato@hotmail.com>
2022-06-20 11:10:11 -0300

    NotePad desenvolvido em Java Projeto simples com as principais funcionalidade do Note Pad
    Tecnologias usada no projeto:
    	- Java - Usei a versão 17
    	- IDE Netbeans - versão 13

---
+++ src/mynotepad/MyNotePad.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mynotepad;
+
+import mynotepad.view.FrmOpen;
+
+/**
+ *
+ * @author RenatoCampos
+ */
+public class MyNotePad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        FrmOpen frmOpen = new FrmOpen();
+        frmOpen.setVisible(true);
+    }
+    
+}
